cbd9fc62bad3e20655ed47183b020468adcdcc6a
Maya Dalton <mad6821@psu.edu>
2024-11-05 11:54:48 -0500

    Updates

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -450,16 +450,14 @@ ggplot(scores) +
 dev.off()
 
 # rename columns
-rownames(scores) <- c("Pre-Arrest Recip", "Post-Arrest ", "Full Graph", 
-                      "Pre-Arrest", "Post-Arrest", "Full Graph")
+rownames(scores) <- c("Pre-Arrest Recip", "Post-Arrest Recip", "Full Graph Recip", 
+                      "Pre-Arrest Trans", "Post-Arrest Trans", "Full Graph Trans")
 
 ## Create table of reciprocity and transitivity scores -----------------------------------------------
 recip_trans_tab <- knitr::kable(scores, booktabs = TRUE, digits=3, format = "latex", 
                                 table.envir = "table", position = "!h",
-                                row.names = c("Pre-Arrest", "Post-Arrest ", "Full Graph", 
-                                              "Pre-Arrest", "Post-Arrest", "Full Graph"),
                                 caption="Reciprocity and Transitivity Scores") %>%
-  pack_rows(index = c("Reciprocity" = 3, "Transitivity" = 3))
+  kableExtra::pack_rows(index = c("Reciprocity" = 3, "Transitivity" = 3))
 
 writeLines(recip_trans_tab, file.path(fig_dir, 'recip_trans_tab.tex')) ## save
 
